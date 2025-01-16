883c36a0b03a598941a31f9076fd49030e4b2bbf
teavver <jerzyx11@gmail.com>
2025-01-16 19:31:29 +0100

    ok this lib suckkkksssss

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
-import imaplib, email, threading, logging
+import imaplib, email, threading, logging, re, sys
 from email.header import decode_header
-from dataclasses import dataclass
 from interval.interval import ThreadJob
 from classes.classes import MailConfig, MailServer
 import util.util as util
+
+# TEMP CONFIG
+MAX_HISTORY = 500
 
 
 def connect_to_gmail(config: MailConfig):
@@ -16,8 +18,23 @@ class MailChecker:
     def __init__(self, mail):
         self.mail = mail
         self.last_email_id = None
+        self.all_email_ids = []
         self.lock = threading.Lock()
+        
+    def search_inbox(self):
+        self.mail.select("inbox", True)
+        # ok, mail_ids = self.mail.search(None, "ALL")
+        # ok, messages = self.mail.search(None, "RECENT")
+        # ok, messages = self.mail.uid("search", None, "ALL")
+        # ok, messages = self.mail.sort("REVERSE DATE", 'UTF-8', "ALL")
+        if ok != "OK":
+            logging.error(f"Failed to search mail inbox")
+            sys.exit(1)
+        # return mail_ids[0].split()[-MAX_HISTORY:]
+        print(len(messages[0]))
+        return messages[0].split()[-MAX_HISTORY:]
 
+    # Detect & match incoming
     def check_for_new_emails(self):
         with self.lock:
             last_email_id = self.last_email_id
@@ -45,9 +62,10 @@ class MailChecker:
             logging.info(f"- new email subject: '{subject}'")
             with self.lock:
                 self.last_email_id = latest_email_id
-
         logging.info("-")
         return last_email_id
+    
+    def check_for_existing_emails(self): pass
 
 
 def main():
@@ -59,15 +77,17 @@ def main():
     mail = connect_to_gmail(config)
     mail_checker = MailChecker(mail)
     event = threading.Event()
+    x = mail_checker.search_inbox()
+    print(len(x))
 
-    try:
-        k = ThreadJob(lambda: mail_checker.check_for_new_emails(), event, 5)
-        k.start()
-        logging.info("- run app ok")
+    # try:
+    #     k = ThreadJob(lambda: mail_checker.check_for_new_emails(), event, 5)
+    #     k.start()
+    #     logging.info("- run app ok")
 
-    except KeyboardInterrupt:
-        logging.info("signing out...")
-        mail.logout()
+    # except KeyboardInterrupt:
+    #     logging.info("signing out...")
+    #     mail.logout()
 
 
 if __name__ == "__main__":
